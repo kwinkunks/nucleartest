212368465d55fd5d68dbc3140ef9fcb1793e7e6c
Matt Hall <matt@agilegeoscience.com>
2017-09-03 09:24:28 -0300

    initial files

---
+++ aznbsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Activate environment
+source /home/nbcommon/anaconda3_410/bin/activate
+
+# Set up proxy
+http_proxy=http://webproxy:3128
+https_proxy=http://webproxy:3128
+export http_proxy 
+export https_proxy
+
+# conda
+conda config --add channels conda-forge
+conda install obspy -y
+conda install basemap -y
+conda install cartopy -y
+
